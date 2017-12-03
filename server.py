ec28f1dd6603baf088336c0aed02fdff37dd5ead
Natanael Ramos <naelr8@gmail.com>
2017-12-03 04:03:22 -0200

    Message on status message

--- server.py
+++ server.py
@@ -7,32 +7,36 @@ import os
 
 PORT = int(os.environ.get("PORT", 5000))
 
-class JSONRequestHandler (BaseHTTPRequestHandler):
+
+class JSONRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
 
         #send response code:
-        self.send_response(200, message = "OK")
+        self.send_response(200, message="HTTP/1.1 200 OK")
         #send headers:
         self.send_header("Content-type", "application/json")
         # send a blank line to end headers:
-        self.wfile.write("\r\n".encode())
+        self.end_headers()
         path = queries = ''
 
         if self.path.startswith('/balance'):
             path = '/balance'
         if self.path.startswith('/balance-history'):
             path = '/balance-history'
-            date_before = (datetime.datetime.now().date() - datetime.timedelta(days = 30)).strftime('%Y%m%d')
+            date_before = (datetime.datetime.now().date() -
+                           datetime.timedelta(days=30)).strftime('%Y%m%d')
             queries = '?date_from=' + date_before
         if self.path.startswith('/transaction-history'):
             path = '/transaction-history'
             curr_date = datetime.datetime.now().date().strftime('%Y%m%d')
-            date_before = (datetime.datetime.now().date() - datetime.timedelta(days = 30)).strftime('%Y%m%d')
+            date_before = (datetime.datetime.now().date() -
+                           datetime.timedelta(days=30)).strftime('%Y%m%d')
             queries = '?date_from=' + date_before + '&date_to=' + curr_date
 
         data = original_api.make_get_request(path, queries)
         self.wfile.write(data.encode())
+
 
 if __name__ == '__main__':
     HOST_NAME = '0.0.0.0'
@@ -46,4 +50,3 @@ if __name__ == '__main__':
         pass
     server.server_close()
     print(datetime.datetime.now(), 'Server Stops - %s:%s' % (HOST_NAME, PORT))
-
