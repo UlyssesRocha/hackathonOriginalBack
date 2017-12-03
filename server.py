0c337615c41e6ca33d4311f6ce123d3d5dd2119b
Natanael Ramos <naelr8@gmail.com>
2017-12-03 03:27:45 -0200

    Fix on the hostname

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ class JSONRequestHandler (BaseHTTPRequestHandler):
         self.wfile.write(data.encode())
 
 if __name__ == '__main__':
-    HOST_NAME = 'localhost'
+    HOST_NAME = 'hackatonoriginalback.herokuapp.com'
     print(HOST_NAME)
     server = HTTPServer((HOST_NAME, PORT), JSONRequestHandler)
     print(datetime.datetime.now(), 'Server Starts - %s:%s' % (HOST_NAME, PORT))
